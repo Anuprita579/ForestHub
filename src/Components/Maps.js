fc1980f80a1c65084465e68d764f0e5898441a92
Anuprita <anupritamhapankar22@gmail.com>
2024-02-04 22:11:10 +0530

    updated commit

--- src/Components/Maps.js
+++ src/Components/Maps.js
@@ -38,7 +38,7 @@ const Maps = () => {
   });
 
   return (
-    <div style={{ height: '400px', width: '100%', background: 'black' }}>
+    <div style={{ height: '400px', width: '100%', background: 'white' }}>
       {forestLocations.length > 0 && (
         <MapContainer center={[forestLocations[0].lat, forestLocations[0].lon]} zoom={12} style={{ height: '100%', width: '100%' }}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
