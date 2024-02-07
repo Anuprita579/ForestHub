4499374a954d48f6486ab48921f6f027da47136f
ShravaniR2412 <shravanirasam0212@gmail.com>
2024-02-07 22:41:24 +0530

    login route added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import Login from "./Components/Login";
-
 export default function App() {
   
   return (
     <div className="w-full h-screen">
-      {/* <Header />
+      <Header />
       <Outlet />
-      <Footer /> */}
-      <Login />
+      <Footer />
 
     </div>
     
