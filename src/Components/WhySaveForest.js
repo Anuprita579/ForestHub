3f58db9f86d5281a4e207c1e78424569811f29f8
Anuprita <anupritamhapankar22@gmail.com>
2024-02-10 11:56:21 +0530

    updated commit

--- src/Components/WhySaveForest.js
+++ src/Components/WhySaveForest.js
@@ -5,7 +5,7 @@ import TreeLearn from './TreeLearn';
 function WhySaveForest() {
   return (
     <>
-    <div className='h-full w-full'>
+    <div className='h-screen w-full'>
     <div className='text-center mt-10'>
         <h1 className='text-4xl font-bold text-green-800'>Nature's SOS</h1>
         <h3 className='p-2 text-md text-green-800'>Understanding why we need to save Forests for Future</h3>
