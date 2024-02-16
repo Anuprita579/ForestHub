f7024cec28c4d83612e3baac5d2cf152c4f58f29
Anuprita <anupritamhapankar22@gmail.com>
2024-02-17 00:25:32 +0530

    feedbackform

--- src/Components/FeedbackForm.js
+++ src/Components/FeedbackForm.js
@@ -123,7 +123,7 @@ function FeedbackForm() {
                 </div>
                 </div>
                 <div className="mt-10">
-                <button className="block w-full rounded-md bg-green-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Submit</button>
+                <button className="block w-full rounded-md bg-green-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Submit </button>
                 </div>
             </form>
             </div> 
