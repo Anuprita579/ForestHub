4d0f10eee6cc37d3a6f7627ba731caf04fb082f9
Anuprita <anupritamhapankar22@gmail.com>
2024-02-19 17:38:18 +0530

    dashboard design

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -94,41 +94,47 @@ function Dashboard() {
 
   return (
     <>
-      <section>
-        <h6 className="text-3xl font-bold mb-4" >Personal Info</h6>
-        <p>Full Name: {userDetails.fullName}</p>
-        <p>Email: {storedEmail}</p>
-      </section>
+      <div className="min-h-screen w-full flex flex-col justify-center items-center">
+        <div className="bg-slate-100 p-5 rounded-xl">
+          <section>
+            <h1 className="text-3xl font-bold mb-4" >Personal Info</h1>
+            <p className="text-xl ">Full Name: <span className="text-lg text-emerald-600">{userDetails.fullName}</span></p>
+            <p className="text-xl ">Email: <span className="text-lg text-emerald-600">{storedEmail}</span> </p>
+          </section>
 
-      <section>
-        <h2 className="text-3xl font-bold mb-4">Blogs</h2>
-        <ul>
-          {blogs.map((blog) => (
-            <li key={blog.id}>
-              <p>Title: {blog.title}</p>
-              <p>Subtitle: {blog.subtitle}</p>
-              <p>Author: {blog.authname}</p>
-              <p>Content: {blog.blogContent}</p>
-            </li>
-          ))}
-        </ul>
-      </section>
-      <section>
-  <h2 className="text-3xl font-bold mb-4">Bookings</h2>
-  <ul>
-    {bookings.map((booking) => (
-      <li key={booking.id}>
-      <p>Full Name: {booking.fullName}</p>
-      <p>Contact No: {booking.contactNo}</p>
-      <p>Location: {booking.eventDetails.location}</p>
-      <p>Title: {booking.eventDetails.title}</p>
-      <p>Date: {booking.eventDetails.date}</p>
-      <p>ID: {booking.eventDetails.id}</p>
-      {/* Add other booking details as needed */}
-    </li>
-    ))}
-  </ul>
-</section>
+          <section>
+            <h2 className="text-3xl font-bold mb-4">Blogs</h2>
+            <ul>
+              {blogs.map((blog) => (
+                <li key={blog.id} className="bg-slate-100 drop-shadow-xl shadow-black transform transition-all duration-150 scale-105 p-4 m-4">
+                  <p>Title: {blog.title}</p>
+                  <p>Subtitle: {blog.subtitle}</p>
+                  <p>Author: {blog.authname}</p>
+                  <p>Content: {blog.blogContent}</p>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section>
+      <h2 className="text-3xl font-bold mb-4">Bookings</h2>
+      <ul>
+        {bookings.map((booking) => (
+          <li key={booking.id} className="bg-slate-100 drop-shadow-xl shadow-black transform transition-all duration-150 scale-105 p-4 m-4">
+          <p>Full Name: {booking.fullName}</p>
+          <p>Contact No: {booking.contactNo}</p>
+          <p>Location: {booking.eventDetails.location}</p>
+          <p>Title: {booking.eventDetails.title}</p>
+          <p>Date: {booking.eventDetails.date}</p>
+          <p>ID: {booking.eventDetails.id}</p>
+          {/* Add other booking details as needed */}
+        </li>
+        ))}
+      </ul>
+    </section>
+
+        </div>
+        
+      </div>
     </>
   );
 }
